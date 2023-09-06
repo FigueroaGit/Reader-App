a6b5a8f937844d26aaf16e7246901f0c6a662b2a
FigueroaGit <hmfp24@gmail.com>
2023-09-06 15:41:18 -0600

    Create Login & Auth Screens/Logic

--- app/src/main/java/com/figueroa/readerapp/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/com/figueroa/readerapp/screens/home/ReaderHomeScreen.kt
@@ -1,9 +1,40 @@
 package com.figueroa.readerapp.screens.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun Home(navController: NavController) {
+fun Home(navController: NavController = NavController(LocalContext.current)) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Top) {
+        Surface(shadowElevation = 8.dp) {
+            TopAppBar(title = { Text(text = "Reader") }, navigationIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Rounded.Menu, contentDescription = "Navigation Drawer Icon")
+                }
+            })
+        }
+        Box(modifier = Modifier.fillMaxSize()) {
+            Text(text = "I'm home")
+        }
 
+    }
 }
